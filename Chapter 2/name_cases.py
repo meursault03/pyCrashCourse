72b007cfe1b2e5dffe78853486a8ef6d6847d63d
João M. Tomaz <jftomaz19@outlook.com>
2025-09-14 21:33:16 -0300

    first chapter complete

---
+++ Chapter 2/name_cases.py
@@ -0,0 +1,17 @@
+message = "The Bad will give his classes today at 10 o'clock, I can't be late."
+print(message)
+someone = "rica games"
+print("Sup " + someone + " are you into the cobra grossa?")
+print("Sup " + someone.title() + " are you into the cobra grossa?")
+print("Sup " + someone.upper() + " are you into the cobra grossa?")
+
+print('"Perfect purity is possible if you turn your life into a line of poetry written with a splash of blood." - Yukio Mishima')
+
+name = "\tErnst\nJunger"
+print(name)
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
+
+filename = "main.py"
+print(filename.removesuffix(".py"))
---
+++ Chapter 2/randomNotes.py
@@ -0,0 +1,9 @@
+numba = 1_590_321
+print(numba.__format__(","))
+print(f"{numba:,}")
+print(3+5)
+''''
+bottom text
+'''
+#npota
+import this
