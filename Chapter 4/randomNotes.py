1230de58a586fda7ba67c7ff8243772119f07914
João M. Tomaz <jftomaz19@outlook.com>
2025-11-05 17:59:57 -0300

    random notes added

---
+++ Chapter 4/randomNotes.py
@@ -0,0 +1,6 @@
+magicians = ["Joe", "Iuri Vale", "Rui"]
+magicians.append("Doctor Professor")
+magicians.insert(0, "David")
+for magician in magicians:
+    print(f"{magician.lower()} é incel")
+    print(f"nice trick {magician.upper()}\n")
