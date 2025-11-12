e17931eb9234ffd967fee9184f64acc6b6df8488
João M. Tomaz <jftomaz19@outlook.com>
2025-11-12 20:50:11 -0300

    few changes in this exercise

--- Chapter 4/pizzaExercise.py
+++ Chapter 4/pizzaExercise.py
@@ -1,7 +1,20 @@
 pizzas = ["pepperoni", "hawaiian", "chicken n cream cheese", "pineapple"]
-for pizza in pizzas:
-    print(f"I like {pizza} pizza")
-print("I love pizza")
+# for pizza in pizzas:
+#     print(f"I like {pizza} pizza")
+# print("I love pizza")
+
+# animals = ["wolves", "leopards", "hippos", "lions"]
+# for animal in animals:
+#     print(animal)
+#     if animal == "wolves":
+#         print("wolves howl a lot")
+#     elif animal == "leopards":
+#         print("pretty sure they meow a little")
+#     elif animal == "hippos":
+#         print("theyre huge")
+#     elif animal == "lions":
+#         print("the lion doesnt concern himself with learning clean code")
+# print("they're all predators")
 
 animals = ["wolves", "leopards", "hippos", "lions"]
 for animal in animals:
