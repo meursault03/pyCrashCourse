88b50fe25e86ccd89dd3b41cc50333388dbde791
João M. Tomaz <jftomaz19@outlook.com>
2025-11-13 18:16:36 -0300

    new exercises from chapter 5

---
+++ Chapter 5/fav_fruit.py
@@ -0,0 +1,9 @@
+favorite_fruits = ["apple","banana","orange"]
+
+if "tangerine" in favorite_fruits:
+    print(f"You dont really like tangerine")
+if "tamarind" in favorite_fruits:
+    print(f"You dont really like tamarind")
+
+for fruit in favorite_fruits:
+    print(f"You really like {fruit}s")
---
+++ Chapter 5/life_phases.py
@@ -0,0 +1,13 @@
+age = 21
+if age < 2:
+    print("You're an infant")
+elif age >= 2 and age < 4:
+    print("You're a child")
+elif age >= 4 and age < 13:
+    print("You're a boy/girl")
+elif age >= 13 and age < 20:
+    print("You're an adolescent")
+elif age >= 20 and age < 65:
+    print("You're an adult")
+else:
+    print("You're an senior")
