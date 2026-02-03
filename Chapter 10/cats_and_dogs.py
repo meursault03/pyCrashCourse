da2e1dfe8415235f852a239189b7e3d0192ff3ee
João M. Tomaz <jftomaz19@outlook.com>
2026-02-03 10:47:30 -0300

    chapter 10 exercises

--- Chapter 10/cats_and_dogs.py
+++ Chapter 10/cats_and_dogs.py
@@ -6,12 +6,12 @@ dog_file = Path('dogs.txt')
 def read_and_print(file_path):
      """Read a file and print its contents line by line."""
      try:
-         path = file_path.read_text()
+         path = file_path.read_text().title()
      except FileNotFoundError:
          print(f"Sorry, the file {file_path} does not exist.")
      else:
          print(path)
 
-read_and_print(cat_file.capitalize())
+read_and_print(cat_file)
 read_and_print(dog_file)
 
