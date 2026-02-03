f5b74d2dfc7c19d5fbec5521247a9d77ba1644f0
João M. Tomaz <jftomaz19@outlook.com>
2026-02-03 11:03:17 -0300

    chapter 10 exercises

--- Chapter 10/cats_and_dogs.py
+++ Chapter 10/cats_and_dogs.py
@@ -2,16 +2,19 @@ from pathlib import Path
 
 cat_file = Path('cats.txt')
 dog_file = Path('dogs.txt')
+vale_file = Path('vales.txt')
 
 def read_and_print(file_path):
      """Read a file and print its contents line by line."""
      try:
          path = file_path.read_text().title()
      except FileNotFoundError:
-         print(f"Sorry, the file {file_path} does not exist.")
+         pass
+     except NameError:
+         print(f"No value found in variable")
      else:
          print(path)
 
 read_and_print(cat_file)
 read_and_print(dog_file)
-
+read_and_print(vale_file)
